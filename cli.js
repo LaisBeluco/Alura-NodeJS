dd3820708ae264405fe1e9c122aaf2c007bf439b
Lais B <laisbelucom@gmail.com>
2022-09-01 20:20:17 -0300

    NodeJS - iniciando curso de NodeJS da Alura

---
+++ cli.js
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+import { pegaArquivo } from "./index.js";
+
+const caminho = process.argv;
+
+async function processaTexto(caminhoDoArquivo) {
+  const resultado = await pegaArquivo(caminhoDoArquivo[2]);
+  console.log(chalk.yellow("lista de links"), resultado);
+}
+processaTexto(caminho);
---
+++ index.js
@@ -0,0 +1,48 @@
+import chalk from "chalk";
+import fs from "fs";
+
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+  const liksExtraidos = regex.exec(texto);
+  return arrayResultados.length === 0 ? "não há links" : arrayResultados;
+}
+
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, "não há arquivo no caminho"));
+}
+
+export async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = "utf-8";
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    return extraiLinks(texto);
+  } catch (erro) {
+    trataErro(erro);
+  } finally {
+    console.log(chalk.yellow("operação concluída"));
+  }
+}
+
+// module.exports = pegaArquivo;
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = "utf-8";
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch((erro) => trataErro(erro));
+// }
+
+// function pegaArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) =>{
+//     if(erro){
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   })}
